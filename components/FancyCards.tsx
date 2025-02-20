3620dbde33b472ae000752d755d0f77ae7da88fd
vinaysaini-here <vinaysaini675859@gmail.com>
2025-02-20 18:54:44 +0530

    Fancy CArd Fixes

--- components/FancyCards.tsx
+++ components/FancyCards.tsx
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
         marginTop: 14,
         padding: 8,
         width : "100%",
-        height : "100%",
+        // height : "100%",
         borderRadius: 12,
         // backgroundColor: "#FBD28B",
         // elevation: 5,
